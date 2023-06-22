28105f586b65d26c1ff9afaaffe578da238498d8
kvit.d.o@gmail.com <kvit.d.o@gmail.com>
2023-06-22 23:08:54 +0300

    init: starting frtch

---
+++ src/TestProvider/FetchTest.js
@@ -0,0 +1,9 @@
+
+function FetchTest(){
+    const fetch = fetch('https://api.github.com/repos/mrshahzeb7/linkedin-skill-assessments-quizzes/contents/javascript/javascript-quiz.md')
+    console.log(fetch)
+}
+
+export default FetchTest
+
+
--- src/components/Tests/Tests.js
+++ src/components/Tests/Tests.js
@@ -1,6 +1,39 @@
 import styles from './Tests.module.css'
+import FetchTest from "../../TestProvider/FetchTest";
+import {useEffect} from "react";
+
+
+// FetchTest()
 
 function Tests(){
+
+    useEffect(()=>{
+        console.log('response')
+
+        const fetchTests = async () =>{
+            let data = await fetch (
+                `https://api.github.com/repos/mrshahzeb7/linkedin-skill-assessments-quizzes/contents/javascript/javascript-quiz.md`
+            )
+            if(!data.ok){
+                throw new Error('Something went wrong');
+            }
+            console.log('data', data)
+
+
+
+            const response = await fetch('https://api.github.com/repos/mrshahzeb7/linkedin-skill-assessments-quizzes/contents/javascript/javascript/javascript-quiz.md')
+
+            if(!response.ok){
+                throw new Error('Something went wrong');
+            }
+            console.log('response', response)
+
+        }
+        fetchTests()
+    },[])
+
+
+
     return(<div className={styles.main} >
             some tests
         </div>
