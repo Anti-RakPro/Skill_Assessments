287517303358459af37ea5c8c60b2258331d264a
kvit.d.o@gmail.com <kvit.d.o@gmail.com>
2023-09-02 18:06:00 +0300

    init: router first steps

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+
+import { Routes, Route, Link } from 'react-router-dom';
+
 import logo from './logo.svg';
 import styles from './App.module.css'
 
@@ -8,16 +11,26 @@ import Tests from "./components/Tests/Tests";
 
 
 function App() {
-    return (<React.Fragment>
-            <div>
-                <Header/>
-                <div className={styles['whole-page']}>
-                    <Body>
-                        <Tests/>
+    return (
+        <React.Fragment>
+            <Routes>
 
-                    </Body>
-                </div>
-            </div>
+                <Route path='/' element={'home'}/>
+                <Route path='/list' element={'list of available tests'} />
+                {/*<Route path='/elm2' element={'element2'} />*/}
+                {/*<Route path='/elm3' element={'element3'} />*/}
+                <Route path={'/test'} element={ <div>
+                        <Header/>
+                        <div className={styles['whole-page']}>
+                            <Body>
+                                <Tests/>
+                            </Body>
+                        </div>
+                    </div>}/>
+
+                <Route path='*' element='page not found' />
+            </Routes>
+
 
     </React.Fragment>
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-
-    <App />
-
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById('root')
 );
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//
+//     <App />
+//
+// );
 
 
