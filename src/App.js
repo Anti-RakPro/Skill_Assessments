400280f77431997278e7ba5dd2907ea3a8f1f694
kvit.d.o@gmail.com <kvit.d.o@gmail.com>
2023-09-03 14:24:18 +0300

    init: core router, +header nav bar

--- src/App.js
+++ src/App.js
@@ -13,23 +13,18 @@ import Tests from "./components/Tests/Tests";
 function App() {
     return (
         <React.Fragment>
-            <Routes>
-                <Route path='/about' element={'About'}/>
-                <Route path='/' element={'Home'}/>
-                <Route path='/list' element={'list of available tests'} />
-                {/*<Route path='/elm2' element={'element2'} />*/}
-                {/*<Route path='/elm3' element={'element3'} />*/}
-                <Route path={'/test'} element={ <div>
-                    <Header/>
-                    <div className={styles['whole-page']}>
-                        <Body>
-                            <Tests/>
-                        </Body>
-                    </div>
-                </div>}/>
-                <Route path='/result' element={'Result'}/>
-                <Route path='*' element='page not found' />
-            </Routes>
+            <Header/>
+            <Body>
+                <Routes>
+                    <Route id='about' path='/about' element={'About'}/>
+                    <Route id='home' path='/' element={'Home'}/>
+                    <Route id='tests' path='/tests' element={'list of available tests'} />
+                    <Route id='test' path='/test' element={<Tests/>}/>
+                    <Route path='test/result' element={'Result'}/>
+                    <Route path='*' element='page not found' />
+                </Routes>
+            </Body>
+
 
 
         </React.Fragment>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,29 @@
 import styles from './Header.module.css'
 import jsLogo from '../../img/logo/images.png'
 import React from "react";
+import { Link } from 'react-router-dom';
 
 
 function Header() {
     return (
-        <div className={styles['header-bg']}>
-            <div className={styles['header-main']}>
-                <img className={styles['topic-logo']} src={jsLogo} alt='' />
-                <p>Skill assessment</p>
+        <div className={styles['header']}>
+            <div className={styles['header-container']}>
+                <div className={styles['header-logo']}>
+                    <div className={styles['logo']} src={jsLogo} alt='' >
+                        SA
+                    </div>
+                    <p>Skill Assessment</p>
+                </div>
+                <div className={styles['nav-bar']}>
+                    <Link to={'tests'}>Tests </Link>
+                    <Link to={'test'}>Test </Link>
+                    <Link to={'about'}>About</Link>
+                    </div>
+
             </div>
-        </div>)
+        </div>
+            )
+
 }
 
 export default Header
