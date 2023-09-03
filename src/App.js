b5ee42f9ab1d9c4a459a6a46c8eb876d4b47aab8
kvit.d.o@gmail.com <kvit.d.o@gmail.com>
2023-09-03 11:16:02 +0300

    fix: app

--- src/App.js
+++ src/App.js
@@ -14,25 +14,25 @@ function App() {
     return (
         <React.Fragment>
             <Routes>
-
-                <Route path='/' element={'home'}/>
+                <Route path='/about' element={'About'}/>
+                <Route path='/' element={'Home'}/>
                 <Route path='/list' element={'list of available tests'} />
                 {/*<Route path='/elm2' element={'element2'} />*/}
                 {/*<Route path='/elm3' element={'element3'} />*/}
                 <Route path={'/test'} element={ <div>
-                        <Header/>
-                        <div className={styles['whole-page']}>
-                            <Body>
-                                <Tests/>
-                            </Body>
-                        </div>
-                    </div>}/>
-
+                    <Header/>
+                    <div className={styles['whole-page']}>
+                        <Body>
+                            <Tests/>
+                        </Body>
+                    </div>
+                </div>}/>
+                <Route path='/result' element={'Result'}/>
                 <Route path='*' element='page not found' />
             </Routes>
 
 
-    </React.Fragment>
+        </React.Fragment>
 
     );
 }
