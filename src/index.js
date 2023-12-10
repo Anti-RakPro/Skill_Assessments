ddbd163b1965f113d69c127fb6c6978a44b3a069
Anti-RakPro <kvit.d.o@gmail.com>
2023-12-10 15:39:40 +0200

    init: 4

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 
-ReactDOM.render(
+// ReactDOM.render(
+//     <Router>
+//         <App />
+//     </Router>,
+//     document.getElementById('root')
+// );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
     <Router>
         <App />
-    </Router>,
-    document.getElementById('root')
-);
+    </Router>
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//
-//     <App />
-//
-// );
+);
 
 
