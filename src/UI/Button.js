609fd05211da274350b2b534a1388ec92e403f18
Anti-RakPro <kvit.d.o@gmail.com>
2023-12-10 14:08:47 +0200

    init: second step

--- src/UI/Button.js
+++ src/UI/Button.js
@@ -1,5 +1,7 @@
-import styles from './Button.module.css';
+// import styles from './Button.module.css';
+import React from 'react';
 import classes from './Button.module.css';
+// import PropTypes from 'prop-types';
 
 function Button(props){
 
@@ -11,6 +13,16 @@ function Button(props){
         <button className={clas} onClick={props.onClick}>{props.children}</button>
     );
 }
+
+
+
+//todo 
+// Button.propTypes = {
+//     onClick: PropTypes.func,
+//     children: PropTypes.func,
+//     isLightsUp: PropTypes.bool.isRequired, // Assuming it should be a boolean
+//     // otherProp: PropTypes.any, // Adjust the type according to your needs
+// };
 
 export default Button;
 
