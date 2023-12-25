ab6602ce77f20ef51a65a5314525021526858aa0
Anti-RakPro <kvit.d.o@gmail.com>
2023-12-25 20:23:18 +0200

    init: before -i rudux

--- src/pages/SignIn SignUp/SignIn.tsx
+++ src/pages/SignIn SignUp/SignIn.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 
 interface SignInProps {
     name: string,
     email: string
 }
-
+const x = 'str';
 const SignIn: React.FC<SignInProps> = ({name,email}) => {
     return <div>SignIn</div>;
 };
