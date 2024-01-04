ac5372f8e31ac4d49e57f3e24e5bc99d1040afb9
Anti-RakPro <kvit.d.o@gmail.com>
2024-01-04 11:35:11 +0200

    init:Registration.css.d.ts

--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import styles from "./Registration.module.css";
+
 // import Registration.module.css as styles from
 interface SignInProps {
   name: string;
   email: string;
 }
+
 const x = "str";
 const Registration: React.FC<SignInProps> = ({ name, email }) => {
-  return <div className={styles["red-border"]}>hello</div>;
+  return (
+    <div className={styles["reg-body"]}>
+      <div className={styles["inputs-body"]}></div>
+      <div className={styles["auth-vars"]}></div>
+    </div>
+  );
 };
-
-// function Registration(){
-//     return 'Registration';
-// }
 export default Registration;
