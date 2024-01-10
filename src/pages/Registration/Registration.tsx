760b7c41a98208ba2d4d11dd7be85858e24471bd
Anti-RakPro <kvit.d.o@gmail.com>
2024-01-10 15:13:38 +0200

    init: registration output console.log

--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useReducer } from "react";
+// import {useReducer} from "react";
 import styles from "./Registration.module.css";
 
 import googleLogo from "../../assets/googleLogo.svg";
@@ -15,8 +16,8 @@ const firebaseConfig = {
   apiKey: "AIzaSyA6eyqAMimEp1ZekdYLv039cOXpam1-A68",
   authDomain: "skill-assessments-a758d.firebaseapp.com",
   projectId: "skill-assessments-a758d",
-  storageBucket: "skill-assessments-a758d.appspot.com",
-  messagingSenderId: "392264449678",
+  // storageBucket: "skill-assessments-a758d.appspot.com",
+  // messagingSenderId: "392264449678",
   appId: "1:392264449678:web:48baab4c1d716870df53df",
   measurementId: "G-ZDYM5XPTLV",
 };
@@ -33,17 +34,63 @@ interface SignInProps {
 
 const x = "str";
 const Registration: React.FC<SignInProps> = ({ name, email }) => {
+  const initialState = {
+    email: "",
+    password: "",
+    repeatPassword: "",
+  };
+
+  const formReducer = (state, action) => {
+    switch (action.type) {
+      case "SET_EMAIL":
+        return { ...state, email: action.email };
+      case "SET_PASSWORD":
+        return { ...state, password: action.password };
+      case "SET_REPEAT_PASSWORD":
+        return { ...state, repeatPassword: action.repeatPassword };
+      default:
+        return state;
+    }
+  };
+
+  const [formData, dispatch] = useReducer(formReducer, initialState);
+
+  console.log(formData);
   return (
     <div className={styles["auth-body"]}>
       <div className={styles["auth-left"]}>
         <h2 className={styles["auth-title"]}>Sign Up to continue</h2>
-        <input className={styles["auth-input"]} placeholder="Email address" />
-        <input className={styles["auth-input"]} placeholder="Password" />
-        <input className={styles["auth-input"]} placeholder="Repeat Password" />
+        <input
+          type="text"
+          className={styles["auth-input"]}
+          placeholder="Email address"
+          onChange={(data) => {
+            dispatch({ type: "SET_EMAIL", email: data.target.value });
+          }}
+        />
+        <input
+          type="password"
+          className={styles["auth-input"]}
+          placeholder="Password"
+          onChange={(data) => {
+            dispatch({ type: "SET_PASSWORD", password: data.target.value });
+          }}
+        />
+        <input
+          type="password"
+          className={styles["auth-input"]}
+          placeholder="Repeat Password"
+          onChange={(data) => {
+            dispatch({
+              type: "SET_REPEAT_PASSWORD",
+              repeatPassword: data.target.value,
+            });
+          }}
+        />
         <button className={styles["auth-sign-button"]}>Sign Up</button>
       </div>
       <div className={styles["auth-right"]}>
-        <button className={styles["mediaBtn"]}>
+        <button type="button" className={styles["mediaBtn"]}>
           <img className={styles["google-img"]} src={googleLogo} alt="" />
           Continue with Google
         </button>
