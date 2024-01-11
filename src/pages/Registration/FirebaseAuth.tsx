8417412753dfeb405de30dc74b768ffa7dec80a2
Anti-RakPro <kvit.d.o@gmail.com>
2024-01-11 20:24:47 +0200

    init: singIn

---
+++ src/pages/Registration/FirebaseAuth.tsx
@@ -0,0 +1,42 @@
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA6eyqAMimEp1ZekdYLv039cOXpam1-A68",
+  authDomain: "skill-assessments-a758d.firebaseapp.com",
+  projectId: "skill-assessments-a758d",
+  // storageBucket: "skill-assessments-a758d.appspot.com",
+  // messagingSenderId: "392264449678",
+  appId: "1:392264449678:web:48baab4c1d716870df53df",
+  measurementId: "G-ZDYM5XPTLV",
+};
+
+// // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// // const analytics = getAnalytics(app);
+// const db = getFirestore(app);
+
+function FirebaseAuth(formData: object) {
+  const auth = getAuth();
+  createUserWithEmailAndPassword(auth, formData.email, formData.password)
+    .then((userCredential) => {
+      // Signed up
+      const user = userCredential.user;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+    });
+
+  console.log("getAuth()", getAuth());
+}
+
+export default FirebaseAuth;
--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -1,39 +1,20 @@
 import React, { useReducer } from "react";
-// import {useReducer} from "react";
 import styles from "./Registration.module.css";
-
 import googleLogo from "../../assets/googleLogo.svg";
+import FirebaseAuth from "./FirebaseAuth";
+import { Link } from "react-router-dom";
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyA6eyqAMimEp1ZekdYLv039cOXpam1-A68",
-  authDomain: "skill-assessments-a758d.firebaseapp.com",
-  projectId: "skill-assessments-a758d",
-  // storageBucket: "skill-assessments-a758d.appspot.com",
-  // messagingSenderId: "392264449678",
-  appId: "1:392264449678:web:48baab4c1d716870df53df",
-  measurementId: "G-ZDYM5XPTLV",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 // import Registration.module.css as styles from
-interface SignInProps {
-  name: string;
-  email: string;
-}
+// interface SignInProps {
+//   name: string;
+//   email: string;
+// }
 
 const x = "str";
-const Registration: React.FC<SignInProps> = ({ name, email }) => {
+
+const Registration: React.FC = () => {
   const initialState: { [key: string]: string } = {
     email: "",
     password: "",
@@ -60,39 +41,92 @@ const Registration: React.FC<SignInProps> = ({ name, email }) => {
 
   const [formData, dispatch] = useReducer(formReducer, initialState);
 
-  console.log(formData);
+  // console.log(formData);
+  const authStatus = "SignUp";
   return (
     <div className={styles["auth-body"]}>
       <div className={styles["auth-left"]}>
-        <h2 className={styles["auth-title"]}>Sign Up to continue</h2>
-        <input
-          type="text"
-          className={styles["auth-input"]}
-          placeholder="Email address"
-          onChange={(data) => {
-            dispatch({ type: "SET_EMAIL", email: data.target.value });
-          }}
-        />
-        <input
-          type="password"
-          className={styles["auth-input"]}
-          placeholder="Password"
-          onChange={(data) => {
-            dispatch({ type: "SET_PASSWORD", password: data.target.value });
-          }}
-        />
-        <input
-          type="password"
-          className={styles["auth-input"]}
-          placeholder="Repeat Password"
-          onChange={(data) => {
-            dispatch({
-              type: "SET_REPEAT_PASSWORD",
-              repeatPassword: data.target.value,
-            });
-          }}
-        />
-        <button className={styles["auth-sign-button"]}>Sign Up</button>
+        {authStatus === "SignIn" && (
+          <div className={styles["auth-singUp"]}>
+            <h2 className={styles["auth-title"]}>Sign In to continue</h2>
+            <input
+              type="text"
+              className={styles["auth-input"]}
+              placeholder="Email address"
+              onChange={(data) => {
+                dispatch({ type: "SET_EMAIL", email: data.target.value });
+              }}
+            />
+            <input
+              type="password"
+              className={styles["auth-input"]}
+              placeholder="Password"
+              onChange={(data) => {
+                dispatch({ type: "SET_PASSWORD", password: data.target.value });
+              }}
+            />
+            <button
+              className={styles["auth-sign-button"]}
+              onClick={() => {
+                FirebaseAuth(formData);
+              }}
+            >
+              Sign In
+            </button>
+            <p className={styles["logIn-text"]}>
+              {"Don't have account yet ?"}
+              <Link className={styles["logIn-text"]} to={"about"}>
+                Sign Up
+              </Link>
+            </p>
+          </div>
+        )}
+        {authStatus === "SignUp" && (
+          <div className={styles["auth-singIn"]}>
+            <h2 className={styles["auth-title"]}>Sign Up to continue</h2>
+            <input
+              type="text"
+              className={styles["auth-input"]}
+              placeholder="Email address"
+              onChange={(data) => {
+                dispatch({ type: "SET_EMAIL", email: data.target.value });
+              }}
+            />
+            <input
+              type="password"
+              className={styles["auth-input"]}
+              placeholder="Password"
+              onChange={(data) => {
+                dispatch({ type: "SET_PASSWORD", password: data.target.value });
+              }}
+            />
+            <input
+              type="password"
+              className={styles["auth-input"]}
+              placeholder="Repeat Password"
+              onChange={(data) => {
+                dispatch({
+                  type: "SET_REPEAT_PASSWORD",
+                  repeatPassword: data.target.value,
+                });
+              }}
+            />
+            <button
+              className={styles["auth-sign-button"]}
+              onClick={() => {
+                FirebaseAuth(formData);
+              }}
+            >
+              Sign Up
+            </button>
+            <p className={styles["logIn-text"]}>
+              Already have an account?
+              <Link className={styles["logIn-text"]} to={"about"}>
+                Log In
+              </Link>
+            </p>
+          </div>
+        )}
       </div>
       <div className={styles["auth-right"]}>
         <button type="button" className={styles["mediaBtn"]}>
